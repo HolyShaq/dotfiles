1b5fcba801dd07c5c29d9828ffc72f5854f77d68
HolyShaq <acebyronehalili@gmail.com>
2026-03-30 20:46:41 +0800

    feat: hledger + starship

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,11 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 #end
 
 fish_add_path /home/holyshaq/.spicetify
-
 # ---
+
+# Environment Variables
+export XDG_CONFIG_HOME="$HOME/.config"
+export LEDGER_FILE=/mnt/things/Finances/2026.journal
 
 # Aliases
 ## Dotfiles
@@ -23,6 +26,17 @@ alias cd='z'
 ## Shorthands
 alias lg='lazygit'
 alias cls='clear'
+alias hl='hledger'
 
 # Initialize zoxide
 zoxide init fish | source
+
+# Initialize starship
+starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/holyshaq/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
